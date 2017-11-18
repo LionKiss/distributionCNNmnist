c9cf091c0c7269700c2b031dacb1fdac2209471a
lionkiss <819789214@qq.com>
2017-11-18 13:58:11 +0800

    错误 tab和空格混合使用

--- CNNtestpy2Distribut.py
+++ CNNtestpy2Distribut.py
@@ -56,7 +56,7 @@ def conv2d(x,W):
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
-	
+    
 
 def main(_):
     ps_hosts = FLAGS.ps_hosts.split(",")
@@ -69,50 +69,50 @@ def main(_):
     if FLAGS.job_name == "ps":
         server.join()
     elif FLAGS.job_name == "worker":
-		mnist = input_data.read_data_sets('mnist_data',one_hot=True)
-		n_batch = mnist.train.num_examples // batch_size
+        mnist = input_data.read_data_sets('mnist_data',one_hot=True)
+        n_batch = mnist.train.num_examples // batch_size
         with tf.device(tf.train.replica_device_setter(
                 worker_device="/job:worker/task:%d" % FLAGS.task_index,
                 cluster=cluster)):
 
             x = tf.placeholder(tf.float32,[None,784])
-			y = tf.placeholder(tf.float32,[None,10])
-			x_image = tf.reshape(x,[-1,28,28,1])
+            y = tf.placeholder(tf.float32,[None,10])
+            x_image = tf.reshape(x,[-1,28,28,1])
 
-			W_conv1 = weight_variable([5,5,1,32])
-			b_conv1 = bias_variable([32])
+            W_conv1 = weight_variable([5,5,1,32])
+            b_conv1 = bias_variable([32])
 
-			h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
-			h_pool1 = max_pool_2x2(h_conv1)
+            h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
+            h_pool1 = max_pool_2x2(h_conv1)
 
-			W_conv2 = weight_variable([5,5,32,64])
-			b_conv2 = bias_variable([64])
+            W_conv2 = weight_variable([5,5,32,64])
+            b_conv2 = bias_variable([64])
 
-			h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
-			h_pool2 = max_pool_2x2(h_conv2)
+            h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
+            h_pool2 = max_pool_2x2(h_conv2)
 
-			W_fc1 = weight_variable([7*7*64,1024])
-			b_fc1 = bias_variable([1024])
+            W_fc1 = weight_variable([7*7*64,1024])
+            b_fc1 = bias_variable([1024])
 
-			h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
-			h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
+            h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 
-			keep_prob = tf.placeholder(tf.float32)
-			h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+            keep_prob = tf.placeholder(tf.float32)
+            h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
 
-			W_fc2 = weight_variable([1024,10])
-			b_fc2 = bias_variable([10])
+            W_fc2 = weight_variable([1024,10])
+            b_fc2 = bias_variable([10])
 
-			prediction = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2) + b_fc2)
+            prediction = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2) + b_fc2)
 
-			cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
-			
-			global_step = tf.Variable(0, name='global_step', trainable=False)
-			train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy，global_step=global_step)
+            cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
+            
+            global_step = tf.Variable(0, name='global_step', trainable=False)
+            train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy，global_step=global_step)
 
 
-			correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
-			accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+            correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
             saver = tf.train.Saver()
             init_op = tf.global_variables_initializer()
@@ -136,10 +136,10 @@ def main(_):
                     while not coord.should_stop():
                         # Run train op
                         for batch in range(n_batch):         
-							batch_xs,batch_ys =  mnist.train.next_batch(batch_size)
-							sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.8})
-						test_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
-						print "Iter " + str(epoch) + ", Testing Accuracy= " + str(test_acc)
+                            batch_xs,batch_ys =  mnist.train.next_batch(batch_size)
+                            sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.8})
+                        test_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
+                        print "Iter " + str(epoch) + ", Testing Accuracy= " + str(test_acc)
 
                 except tf.errors.OutOfRangeError:
                     print("Done training after reading all data")
