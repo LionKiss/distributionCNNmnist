bc13d3130ae2b1c78b82e166f3c607a079ddffff
lionkiss <819789214@qq.com>
2017-11-19 21:53:13 +0800

    122

--- CNNtestpy2Distribut.py
+++ CNNtestpy2Distribut.py
@@ -142,7 +142,7 @@ def main(_):
                             batch_xs,batch_ys =  mnist.train.next_batch(batch_size)
                             sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.8})
                         
-                        step,test_acc = sess.run(global_step,accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
+                        test_acc,step,_ = sess.run(accuracy,global_step,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
                         
                         print "global_step" + str(global_step)
                         print "Iter " + str(step) + ", Testing Accuracy= " + str(test_acc)
