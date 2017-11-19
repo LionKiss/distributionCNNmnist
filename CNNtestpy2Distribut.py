55f7c2ad1996ed8a12dbb6a1de116ebb2c831f84
lionkiss <819789214@qq.com>
2017-11-19 22:48:38 +0800

    1

--- CNNtestpy2Distribut.py
+++ CNNtestpy2Distribut.py
@@ -140,7 +140,7 @@ def main(_):
                         # Run train op
                         for batch in range(n_batch):         
                             batch_xs,batch_ys =  mnist.train.next_batch(batch_size)
-                            _,loss_value, step=sess.run([train_step,global_step],feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.8})
+                            _,loss_value, step=sess.run([train_step,cross_entropy,global_step],feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.8})
                         step = step // n_batch
                         test_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
                         
